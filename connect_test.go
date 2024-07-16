f31452c9d5b3d55c3c4c237dbf049b7f18305c1c
JiangXin <jx178861659@163.com>
2024-07-16 11:31:11 +0800

    update

---
+++ connect_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestConnect(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{name: "success connect", args: args{url: "https://www.baidu.com"}, want: true},
+		{name: "failed connect", args: args{url: "www.baidu.com"}, want: false},
+		{name: "bad connect", args: args{url: "http://www.baidu.com"}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			if got := Connect(tt.args.url); got != tt.want {
+				t.Errorf("Connect() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
