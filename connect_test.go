2a6a0d05d378ac40f19770fb26164442fbb987d8
JiangXin <jx178861659@163.com>
2024-07-16 11:41:38 +0800

    update

--- connect_test.go
+++ connect_test.go
@@ -17,6 +17,7 @@ func TestConnect(t *testing.T) {
 		{name: "success connect", args: args{url: "https://www.baidu.com"}, want: true},
 		{name: "failed connect", args: args{url: "www.baidu.com"}, want: false},
 		{name: "bad connect", args: args{url: "http://www.baidu.com"}, want: true},
+		{name: "misguided attempt", args: args{url: "http://www.baidu.com"}, want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
